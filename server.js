939a67c1a9509a0023989f5eb01222b46cd84fe6
Huzaifa785 <huzaifa.coder785@gmail.com>
2022-08-06 15:03:10 +0530

    Removed production logic

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 var cors = require('cors')
-const path = require("path");
 
 const app = express();
 
@@ -47,15 +46,6 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/api/plaid", plaid);
-
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  }
-  );
-}
 
 
 app.listen(PORT, () => console.log(`Server running on PORT:${PORT} 🎉`));
