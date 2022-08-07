a19b0f748ff646db6cab079bd185efcc0c736c96
Huzaifa785 <huzaifa.coder785@gmail.com>
2022-08-07 08:39:06 +0530

    minor changes

--- server.js
+++ server.js
@@ -35,6 +35,11 @@ try {
   console.log(err.message);
 }
 
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the transactions unified backend..." });
+}
+);
+
 // Passport middleware
 app.use(passport.initialize());
 // Passport config
@@ -43,11 +48,14 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/plaid", plaid);
 
-app.use(express.static(path.join(__dirname, "/client/build")));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-});
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  }
+  );
+}
 
 
 app.listen(PORT, () => console.log(`Server running on PORT:${PORT} 🎉`));
