142745ae9b03201211d27137c6e40592d7127588
Huzaifa785 <huzaifa.coder785@gmail.com>
2022-08-07 08:59:54 +0530

    minor changes

--- server.js
+++ server.js
@@ -43,14 +43,11 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/plaid", plaid);
 
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  }
-  );
-}
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 
 app.listen(PORT, () => console.log(`Server running on PORT:${PORT} 🎉`));
